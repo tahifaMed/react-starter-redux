270e597714b0328ae82ab94f5d47a50d9146c285
tahifa Med <tahifa.med@gmail.com>
2018-07-08 12:14:20 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+import reducers from './reducers';
+import './index.css';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
